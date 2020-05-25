b3acd79cf4c052e41d54e3bcea526c05d5290d2b
YULAI LI <ylii0235@student.monash.edu>
2020-05-26 00:59:51 +1000

    refinement

--- Project/HTML/JS/STF_map.js
+++ Project/HTML/JS/STF_map.js
@@ -1,10 +1,10 @@
 mapboxgl.accessToken = "pk.eyJ1Ijoicm95eXVsIiwiYSI6ImNrYTUxb3FyZjA3OW0zZnA0dm9kc3g4M24ifQ.CddGHFtKeVdpE02MP4n4gg";
 const DARKSKY_KEY = "0e195afd5dd4381cf43dc3f53ea53094";
 let map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-        center: [145.135844,-37.911103], // starting position [lng, lat]
-        zoom: 11 // starting zoom
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [145.135844,-37.911103],
+        zoom: 11
 });
 
 function positionCallback(position)
@@ -67,22 +67,4 @@ function airportCallback(data)
   	marker.addTo(map);
   	popup.addTo(map);
   }
-}
-
-
-function search()
-{
-  let fleetInformation = {Date:"",Time:"",Country:"",Airport:""};
-  let dateRef=document.getElementById("date");
-  let timeRef=document.getElementById("time");
-  let countryRef = document.getElementById ("country");
-  let airportRef = document.getElementById("airport");
-
-
-  //fleetInformation.Date=dateRef.value
-  //fleetInformation.Time=timeRef.value}
-  //fleetInformation.Country = countryRef.value
-  //fleetInformation.Airport= airportRef.value
-
-
 }
