02db5c08142379a51828864cf87f65521b394348
YULAI LI <ylii0235@student.monash.edu>
2020-05-26 00:13:25 +1000

    PantoAirport an other function

--- Project/HTML/JS/RTF_map.js
+++ Project/HTML/JS/RTF_map.js
@@ -1,19 +1,19 @@
-
 mapboxgl.accessToken = "pk.eyJ1Ijoicm95eXVsIiwiYSI6ImNrYTUxb3FyZjA3OW0zZnA0dm9kc3g4M24ifQ.CddGHFtKeVdpE02MP4n4gg";
 const DARKSKY_KEY = "0e195afd5dd4381cf43dc3f53ea53094";
 let map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-  center: [145.135844,-37.911103], // starting position [lng, lat]
-  zoom: 11 // starting zoom
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [145.135844,-37.911103],
+  zoom: 11
 })
+
 if('geolocation' in navigator)
 {
   navigator.geolocation.getCurrentPosition(positionCallback);
 }
 else
 {
-  alert("geolocation IS NOT available");
+  alert("Geolocation Is Not Available");
 }
 
 function positionCallback(position)
@@ -22,7 +22,8 @@ function positionCallback(position)
   let lng = position.coords.longitude;
   map.panTo([lng,lat]);
   let url ="https://eng1003.monash/api/v1/darksky/";
-  let data ={
+  let data =
+  {
     u: "ylii0235",
     key: DARKSKY_KEY,
     lat: lat,
@@ -34,27 +35,11 @@ function positionCallback(position)
 
 function weatherCallback(weather)
 {
-  if(weather.daily.summary=)//TypeOfWeather)
-  {
-    //Picture
-  }
+  //show weather
 }
 
-function showFleetInformation()
-{
-  let fleetInformation = JOSN.parse(localStorage.getItem("fleet-data"))
-}
 
-class Time
-{
-  constructor(date)
-  {
-    this._date =""
-  }
-  get date(){return this._date;}
 
-  set date(newDate){this._date = newDate;}
-}
 
 class RouteList
 {
@@ -208,5 +193,20 @@ class CoordinateOfLocation
 	  toString()
 	  {}
   }
+}
 
+class Time
+{
+  constructor(date)
+  {
+    this._date =""
+  }
+  get date(){return this._date;}
+
+  set date(newDate){this._date = newDate;}
+}
+
+function storeFleetInformation()
+{
+  let fleetInformation = JOSN.parse(localStorage.getItem("fleet-data"))
 }
--- Project/HTML/JS/STF_map.js
+++ Project/HTML/JS/STF_map.js
@@ -5,39 +5,81 @@ let map = new mapboxgl.Map({
         style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
         center: [145.135844,-37.911103], // starting position [lng, lat]
         zoom: 11 // starting zoom
-})
+});
 
-/*function getInformationFromCustomer()
+function positionCallback(position)
 {
-  let fleetInformation = {Date:"",Time:"",Country:"",Airport:""};
-  let dateRef=document.getElementById("date")
-  let timeRef=document.getElementById("time")
-  let countryRef = document.getElementById ("country");
-  let airportRef = document.getElementById("airport")
-
-
-  fleetInformation.Date=dateRef.value
-  fleetInformation.Time=timeRef.value}
-  fleetInformation.Country = countryRef.value
-  fleetInformation.Airport= airportRef.value
-
-  if (typeof(Storage) !== "undefined")
+  let countryRef=document.getElementById("country");
+  let cityRef=document.getElementById("city");
+  let country=countryRef.value;
+  let city =cityRef.value;
+  let url ="https://eng1003.monash/api/v1/airports/";
+  let data =
   {
-  	localStorage.setItem("fleet-data",JSON.stringify(fleetInformation));
-  }
-  else
+    u: "ylii0235",
+    key: DARKSKY_KEY,
+    country:country,
+    city:city,
+    callback: "airportCallback"
+  };
+  webServiceRequest(url,data);
+}
+function webServiceRequest(url,data)
+{
+    let params = "";
+    for (let key in data)
+    {
+        if (data.hasOwnProperty(key))
+        {
+            if (params.length == 0)
+            {
+                params += "?";
+            }
+            else
+            {
+                params += "&";
+            }
+
+            let encodedKey = encodeURIComponent(key);
+            let encodedValue = encodeURIComponent(data[key]);
+
+            params += encodedKey + "=" + encodedValue;
+         }
+    }
+    let script = document.createElement('script');
+    script.src = url + params;
+    document.body.appendChild(script);
+}
+
+function airportCallback(data)
+{
+  console.log(data);
+  for (let i = 0; i < data.length; i++)
   {
-  	console.log("localStorage is not supported by current browser.");
+  	let marker = new mapboxgl.Marker({ "color": "#FF8C00" });
+  	marker.setLngLat(data.longitude,data.latitude);
+
+  	let popup = new mapboxgl.Popup({ offset: 45});
+  	popup.setText(data.airportCode);
+
+  	marker.setPopup(popup);
+
+  	// Display the marker.
+  	marker.addTo(map);
+
+  	// Display the popup.
+  	popup.addTo(map);
   }
-}*/
+}
+
 
 function search()
 {
   let fleetInformation = {Date:"",Time:"",Country:"",Airport:""};
-  let dateRef=document.getElementById("date")
-  let timeRef=document.getElementById("time")
+  let dateRef=document.getElementById("date");
+  let timeRef=document.getElementById("time");
   let countryRef = document.getElementById ("country");
-  let airportRef = document.getElementById("airport")
+  let airportRef = document.getElementById("airport");
 
 
   //fleetInformation.Date=dateRef.value
