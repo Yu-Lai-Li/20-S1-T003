e7329ad112ce8c4742fed5097b64d7bb3db31d22
YULAI LI <ylii0235@student.monash.edu>
2020-05-25 19:01:00 +1000

    indent

--- Project/HTML/JS/RTF_map.js
+++ Project/HTML/JS/RTF_map.js
@@ -34,144 +34,144 @@ function positionCallback(position)
 
 function weatherCallback(weather)
 {
-  if(weather.daily.summary=//TypeOfWeather)
-    {
-      //Picture
-    }
-  }
-
-  function showFleetInformation()
+  if(weather.daily.summary=)//TypeOfWeather)
   {
-    let fleetInformation = JOSN.parse(localStorage.getItem("fleet-data"))
+    //Picture
   }
+}
 
-  class Time
+function showFleetInformation()
+{
+  let fleetInformation = JOSN.parse(localStorage.getItem("fleet-data"))
+}
+
+class Time
+{
+  constructor(date)
   {
-    constructor(date)
-    {
-      this._date =""
-    }
-    get date(){return this._date;}
-
-    set date(newDate){this._date = newDate;}
+    this._date =""
   }
+  get date(){return this._date;}
 
-  class RouteList
+  set date(newDate){this._date = newDate;}
+}
+
+class RouteList
+{
+  constructor(origin="",destination="",waypoints="",arrivalTime=0,departureTime=0,totalDistance=0)
   {
-    constructor(origin="",destination="",waypoints="",arrivalTime=0,departureTime=0,totalDistance=0)
-    {
-      this._origin=origin;
-      this._destination=destination;
-      this._waypoints=waypoints;//waypoints in array list
-      this._arrivalTime=arrivalTime;
-      this._departureTime=departureTime;
-      this._totalDistance=totalDistance;
-    }
-
-    get origin() {return this._origin;}
-    get destination() {return this._destination;}
-    get waypoints() {return this._waypoints;}
-    get numberOfWaypoints() {return this._waypoints.length;}
-    get arrivalTime() {return this._arrivalTime;}
-    get departureTime() {return this._departureTime;}
-    get totalDistance() {return this._totalDistance;}
-
-    set origin(newOrigin) { this._origin=newOrigin;}
-    set destination(newDestination) {this._destination=newDestination;}
-    set waypoints(newWaypoints) { this._waypoints=newWaypoints;}
-    set arrivalTime(newArrivalTime) { this._arrivalTime=newArrivalTime;}
-    set departureTime(newDepartureTime) {this._departureTime=newDepartureTime;}
-
-    addWaypoints(waypoints){}
-    deleteRoute(){}
-    displayRoutes(){}
-    dispaydepature(){}
-    dispayArrivalTime(){}
-    toString(){}
-
+    this._origin=origin;
+    this._destination=destination;
+    this._waypoints=waypoints;//waypoints in array list
+    this._arrivalTime=arrivalTime;
+    this._departureTime=departureTime;
+    this._totalDistance=totalDistance;
   }
 
-  class WeatherInformation
+  get origin() {return this._origin;}
+  get destination() {return this._destination;}
+  get waypoints() {return this._waypoints;}
+  get numberOfWaypoints() {return this._waypoints.length;}
+  get arrivalTime() {return this._arrivalTime;}
+  get departureTime() {return this._departureTime;}
+  get totalDistance() {return this._totalDistance;}
+
+  set origin(newOrigin) { this._origin=newOrigin;}
+  set destination(newDestination) {this._destination=newDestination;}
+  set waypoints(newWaypoints) { this._waypoints=newWaypoints;}
+  set arrivalTime(newArrivalTime) { this._arrivalTime=newArrivalTime;}
+  set departureTime(newDepartureTime) {this._departureTime=newDepartureTime;}
+
+  addWaypoints(waypoints){}
+  deleteRoute(){}
+  displayRoutes(){}
+  dispaydepature(){}
+  dispayArrivalTime(){}
+  toString(){}
+
+}
+
+class WeatherInformation
+{
+  constructor(location,temperature,typeOfWeather)
   {
-    constructor(location,temperature,typeOfWeather)
-    {
-       this._location=location;
-       this._temperature= temperature;
-       this._typeOfWeather=typeOfWeather;
-    }
-    	  get location(){return this._location;}
-    	  get temperature(){return this._temperature;}
-    	  get typeOfWeather(){return this._typeOfWeather;}
-
-    	  set location(newLocation){this._location=newLocation;}
-    	  set temperature(newTemperature){this._temperature=newTemperature;}
-    	  set typeOfWeather(newType){this._typeOfWeather=newType;}
-
+    this._location=location;
+    this._temperature= temperature;
+    this._typeOfWeather=typeOfWeather;
   }
+  get location(){return this._location;}
+  get temperature(){return this._temperature;}
+  get typeOfWeather(){return this._typeOfWeather;}
 
-  class PlaneList
+  set location(newLocation){this._location=newLocation;}
+  set temperature(newTemperature){this._temperature=newTemperature;}
+  set typeOfWeather(newType){this._typeOfWeather=newType;}
+
+}
+
+class PlaneList
+{
+  constructor(numberOfAirplane,stateOfAirplane)
   {
-    constructor(numberOfAirplane,stateOfAirplane)
-    {
-      this._numberOfAirplane=numberOfAirplane;
-      this._stateOfAirplane=stateOfAirplane;
-    }
-      get numberOfAirplane(){return this._numberOfAirplane;}
-      get stateOfAirplane(){return this._stateOfAirplane;}
-
-      set numberOfAirplane(newNumber){this._numberOfAirplane=newNumber;}
-      set stateOfAirplane(newState){this._stateOfAirplane=newState;}
+    this._numberOfAirplane=numberOfAirplane;
+    this._stateOfAirplane=stateOfAirplane;
   }
+  get numberOfAirplane(){return this._numberOfAirplane;}
+  get stateOfAirplane(){return this._stateOfAirplane;}
 
-  class InformationOfAirplane
+  set numberOfAirplane(newNumber){this._numberOfAirplane=newNumber;}
+  set stateOfAirplane(newState){this._stateOfAirplane=newState;}
+}
+
+class InformationOfAirplane
+{
+  constructor(model,speed,manufacturer,company,availability, capacity,flyingRange)
   {
-    constructor(model,speed,manufacturer,company,availability, capacity,flyingRange)
-     {
-        this._model = model;
-        this._speed = speed;
-        this._manufacturer=manufactuer;
-        this._company=company;
-        this._availability=availability;
-        this._capacity=capacity;
-        this._flyingRange=flyingRange;
-        this._timeNeeded =0;
-      }
-        get model(){return this._model;}
-        get speed(){return this._speed;}
-        get manufactuer(){return this._manufacturer;}
-        get company(){return this._company;}
-        get availability(){return this._availability;}
-        get capacity(){return this._capacity;}
-        get flyingRange(){return this.flyingRange;}
-        get timeNeeded(){return this._timeNeeded;}
-
-        set model(newModel){this._model=newModel;}
-        set speed(newSpeed){this._speed=newSpeed;}
-        set manufactuer(newManufacturer){ this._manufacturer=newManufacturer;}
-        set company(newCompany){ this._company=newCompany;}
-        set availability(newAvailabilty){ this._availability=newAvailabilty;}
-        set capacity(newCapacity){ this._capacity=newCapacity;}
-        set flyingRange(newFlyingRange){this.flyingRange=newFlyingRange;}
-        set timeNeeded(newTime){this._timeNeeded= newTime;}
-
-        addFlights(){}
-        toString(){}
-        displayInformation(){}
-
+    this._model = model;
+    this._speed = speed;
+    this._manufacturer=manufactuer;
+    this._company=company;
+    this._availability=availability;
+    this._capacity=capacity;
+    this._flyingRange=flyingRange;
+    this._timeNeeded =0;
   }
+  get model(){return this._model;}
+  get speed(){return this._speed;}
+  get manufactuer(){return this._manufacturer;}
+  get company(){return this._company;}
+  get availability(){return this._availability;}
+  get capacity(){return this._capacity;}
+  get flyingRange(){return this.flyingRange;}
+  get timeNeeded(){return this._timeNeeded;}
 
-  class CooridinateOfLocation
+  set model(newModel){this._model=newModel;}
+  set speed(newSpeed){this._speed=newSpeed;}
+  set manufactuer(newManufacturer){ this._manufacturer=newManufacturer;}
+  set company(newCompany){ this._company=newCompany;}
+  set availability(newAvailabilty){ this._availability=newAvailabilty;}
+  set capacity(newCapacity){ this._capacity=newCapacity;}
+  set flyingRange(newFlyingRange){this.flyingRange=newFlyingRange;}
+  set timeNeeded(newTime){this._timeNeeded= newTime;}
+
+  addFlights(){}
+  toString(){}
+  displayInformation(){}
+
+}
+
+class CooridinateOfLocation
+{
+  constructor(lng1,lat1,alt1,lng2,lat2,alt2)
   {
-    constructor(lng1,lat1,alt1,lng2,lat2,alt2)
-    {
-      this._location1=[lng1,lat1,alt1];
-      this._location2=[lng2,lat2,alt2];
-      this._distance =0;
-    }
-      get location1(){return this._location1;}
-      get location2(){return this._location2;}
-      get distance(){return this._distance;}
-
-
-      set distance(newDistance){this._distance=newDistance;}
+    this._location1=[lng1,lat1,alt1];
+    this._location2=[lng2,lat2,alt2];
+    this._distance =0;
   }
+  get location1(){return this._location1;}
+  get location2(){return this._location2;}
+  get distance(){return this._distance;}
+
+
+  set distance(newDistance){this._distance=newDistance;}
+}
